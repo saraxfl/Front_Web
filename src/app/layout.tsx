8515f8dfd8e8c4a7ddc20a4c2a9da9627654b181
saraxfl <A07045944@tec.mx>
2025-10-26 21:26:02 -0600

    Move Font Awesome link to head section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import "simple-datatables/dist/style.css";
+
 import { AuthProvider } from "@/context/AuthContext";
 import ClientBootstrap from "./ClientBootstrap";
-<link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-/>
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        />
+      </head>
       <body>
         <AuthProvider>
           {children}
-          <ClientBootstrap /> {/* habilita dropdowns, tooltips, etc. (cliente) */}
+          <ClientBootstrap />
         </AuthProvider>
       </body>
     </html>
   );
 }
-
